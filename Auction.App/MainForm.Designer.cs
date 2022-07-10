c5b896df382d7bcb034ad5771d45d335f9e1fe52
Ilya Kramarev <ikramarev01@gmail.com>
2022-07-11 00:18:31 +0300

    add buttons

--- Auction.App/MainForm.Designer.cs
+++ Auction.App/MainForm.Designer.cs
@@ -28,12 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.task1Button = new System.Windows.Forms.Button();
+            this.task2Button = new System.Windows.Forms.Button();
+            this.task3Button = new System.Windows.Forms.Button();
+            this.task4Button = new System.Windows.Forms.Button();
+            this.task5Button = new System.Windows.Forms.Button();
+            this.task6Button = new System.Windows.Forms.Button();
+            this.task7Button = new System.Windows.Forms.Button();
+            this.task8Button = new System.Windows.Forms.Button();
+            this.task9Button = new System.Windows.Forms.Button();
+            this.task10Button = new System.Windows.Forms.Button();
+            this.task11Button = new System.Windows.Forms.Button();
+            this.task12Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // task1Button
+            // 
+            this.task1Button.Location = new System.Drawing.Point(12, 12);
+            this.task1Button.Name = "task1Button";
+            this.task1Button.Size = new System.Drawing.Size(377, 23);
+            this.task1Button.TabIndex = 0;
+            this.task1Button.Text = "Список аукционов для интервала дат";
+            this.task1Button.UseVisualStyleBackColor = true;
+            // 
+            // task2Button
+            // 
+            this.task2Button.Location = new System.Drawing.Point(12, 41);
+            this.task2Button.Name = "task2Button";
+            this.task2Button.Size = new System.Drawing.Size(377, 23);
+            this.task2Button.TabIndex = 1;
+            this.task2Button.Text = "Добавить предмет на аукцион";
+            this.task2Button.UseVisualStyleBackColor = true;
+            // 
+            // task3Button
+            // 
+            this.task3Button.Location = new System.Drawing.Point(12, 70);
+            this.task3Button.Name = "task3Button";
+            this.task3Button.Size = new System.Drawing.Size(377, 23);
+            this.task3Button.TabIndex = 2;
+            this.task3Button.Text = "Список аукционов с суммарным доходом";
+            this.task3Button.UseVisualStyleBackColor = true;
+            // 
+            // task4Button
+            // 
+            this.task4Button.Location = new System.Drawing.Point(12, 99);
+            this.task4Button.Name = "task4Button";
+            this.task4Button.Size = new System.Drawing.Size(377, 23);
+            this.task4Button.TabIndex = 3;
+            this.task4Button.Text = "Список предметов, проданных за интервал дат";
+            this.task4Button.UseVisualStyleBackColor = true;
+            // 
+            // task5Button
+            // 
+            this.task5Button.Location = new System.Drawing.Point(12, 128);
+            this.task5Button.Name = "task5Button";
+            this.task5Button.Size = new System.Drawing.Size(377, 23);
+            this.task5Button.TabIndex = 4;
+            this.task5Button.Text = "Совершить продажу предмета на аукционе";
+            this.task5Button.UseVisualStyleBackColor = true;
+            // 
+            // task6Button
+            // 
+            this.task6Button.Location = new System.Drawing.Point(12, 157);
+            this.task6Button.Name = "task6Button";
+            this.task6Button.Size = new System.Drawing.Size(377, 23);
+            this.task6Button.TabIndex = 5;
+            this.task6Button.Text = "Список продавцов и общая сумма от продаж за интервал дат";
+            this.task6Button.UseVisualStyleBackColor = true;
+            // 
+            // task7Button
+            // 
+            this.task7Button.Location = new System.Drawing.Point(12, 186);
+            this.task7Button.Name = "task7Button";
+            this.task7Button.Size = new System.Drawing.Size(377, 23);
+            this.task7Button.TabIndex = 6;
+            this.task7Button.Text = "Список покупателей, купивших что-то за интервал дат";
+            this.task7Button.UseVisualStyleBackColor = true;
+            // 
+            // task8Button
+            // 
+            this.task8Button.Location = new System.Drawing.Point(12, 215);
+            this.task8Button.Name = "task8Button";
+            this.task8Button.Size = new System.Drawing.Size(377, 23);
+            this.task8Button.TabIndex = 7;
+            this.task8Button.Text = "Создать аукцион";
+            this.task8Button.UseVisualStyleBackColor = true;
+            // 
+            // task9Button
+            // 
+            this.task9Button.Location = new System.Drawing.Point(12, 244);
+            this.task9Button.Name = "task9Button";
+            this.task9Button.Size = new System.Drawing.Size(377, 23);
+            this.task9Button.TabIndex = 8;
+            this.task9Button.Text = "Список аукционов для указанного места";
+            this.task9Button.UseVisualStyleBackColor = true;
+            // 
+            // task10Button
+            // 
+            this.task10Button.Location = new System.Drawing.Point(12, 273);
+            this.task10Button.Name = "task10Button";
+            this.task10Button.Size = new System.Drawing.Size(377, 23);
+            this.task10Button.TabIndex = 9;
+            this.task10Button.Text = "Список продавцов, участвовавших в торгах за период времени";
+            this.task10Button.UseVisualStyleBackColor = true;
+            // 
+            // task11Button
+            // 
+            this.task11Button.Location = new System.Drawing.Point(12, 302);
+            this.task11Button.Name = "task11Button";
+            this.task11Button.Size = new System.Drawing.Size(377, 23);
+            this.task11Button.TabIndex = 10;
+            this.task11Button.Text = "Информация о продавцах и покупателях";
+            this.task11Button.UseVisualStyleBackColor = true;
+            // 
+            // task12Button
+            // 
+            this.task12Button.Location = new System.Drawing.Point(12, 331);
+            this.task12Button.Name = "task12Button";
+            this.task12Button.Size = new System.Drawing.Size(377, 23);
+            this.task12Button.TabIndex = 11;
+            this.task12Button.Text = "Списоку покупателей за указанный срок";
+            this.task12Button.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainForm";
+            this.ClientSize = new System.Drawing.Size(398, 364);
+            this.Controls.Add(this.task12Button);
+            this.Controls.Add(this.task11Button);
+            this.Controls.Add(this.task10Button);
+            this.Controls.Add(this.task9Button);
+            this.Controls.Add(this.task8Button);
+            this.Controls.Add(this.task7Button);
+            this.Controls.Add(this.task6Button);
+            this.Controls.Add(this.task5Button);
+            this.Controls.Add(this.task4Button);
+            this.Controls.Add(this.task3Button);
+            this.Controls.Add(this.task2Button);
+            this.Controls.Add(this.task1Button);
+            this.Name = "MainForm";
+            this.Text = "Аукционы";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button task1Button;
+        private Button task2Button;
+        private Button task3Button;
+        private Button task4Button;
+        private Button task5Button;
+        private Button task6Button;
+        private Button task7Button;
+        private Button task8Button;
+        private Button task9Button;
+        private Button task10Button;
+        private Button task11Button;
+        private Button task12Button;
     }
 }
